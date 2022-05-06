f929140dfb5d6fa7c5acf99436f04e39a18b4f45
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-05-05 20:53:11 -0500

    Create BlackJackPlayer.java

---
+++ API/src/game/BlackJackPlayer.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.ArrayList;
+
+public abstract class BlackJackPlayer extends Player {
+
+	public BlackJackPlayer(ArrayList<Card> p_hand, String name, int numPlayer) {
+		super(p_hand, name, numPlayer);
+	}
+	
+	public abstract void drawCard();
+	public abstract void passTurn();
+
+}
