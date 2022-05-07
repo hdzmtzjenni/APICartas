fe7a5e5341d9acee02881ed183421a7266c087b2
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-05-07 01:59:19 -0500

    Pruebas

--- API/src/Test.java
+++ API/src/Test.java
@@ -1,13 +1,32 @@
+import game.BlackJackGame;
+import game.CardGame;
 import game.PackOfCards;
+import game.Player;
+import game.PokerGame;
+import game.UserPlayer;
 
 public class Test {
 
 	public static void main(String[] args) {
-		PackOfCards a = new PackOfCards();
-		a.startPack();
-		System.out.println(a.getPack().toString());
-		a.sortCards();
-		System.out.println(a.getPack().toString());
+		// PackOfCards a = new PackOfCards();
+		// a.startPack();
+		// System.out.println(a.getPack().toString());
+		// a.sortCards();
+		// System.out.println(a.getPack().toString());
+		
+		// System.out.println("\n\n\n");
+
+		CardGame blackjack = new BlackJackGame();
+		Player player4 = BlackJackGame.createIntelligentPlayer();
+		Player player5 = ;
+		Player player6 = new UserPlayer();
+		CardGame poker = new PokerGame();
+		poker.addPlayer("oli", player4);
+		blackjack.addPlayer("John", player4);
+		blackjack.addPlayer("Mary", player5);
+		blackjack.addPlayer("Joseph",player6);
+		blackjack.addPlayer("Anna",BlackJackGame.createIntelligentPlayer());
+		System.out.println(BlackJackGame.playerList.size());
 		
 	}
 
