bbc87a24da98e729658e9c5ea958fb2d465f3962
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-05-08 22:32:24 -0500

    Update UserPlayer.java

--- API/src/game/UserPlayer.java
+++ API/src/game/UserPlayer.java
@@ -12,25 +12,26 @@ public class UserPlayer extends Player{
 	@Override
 	public Boolean play() {
 	
-		while (!handReady) {
+		while (!handReady) {//Mientras el jugador no termine su turno
 			System.out.printf("\nPlayer %s",this.name);
 			System.out.printf("\nHand: %d\n",this.sumOfHand());
 			System.out.println(this.getP_hand());
 		
-			if (this.sumOfHand()==21) {
+			if (this.sumOfHand()==21) {//Si la suma de sus cartas es 21 el jugador gana y se acaba su turno
 				System.out.printf("%s tiene BlackJack",this.name);
 				this.handReady = true;
 				this.turn++;
 				break;
 			}
 
-			if (this.sumOfHand()>21) {
+			if (this.sumOfHand()>21) {//Si la suma es más de 21 el jugador pierde y se acaba su turno
 				System.out.printf("Tiene más de 21, %s pierde",this.name);
 				this.handReady=true;
 				this.turn++;
 				return false;
 			}
 			
+			//El jugador decide si agarra otra carta o si se queda y acaba su turno
 			String action = JOptionPane.showInputDialog("Stand(S) -- Draw a card(D):");
 			action = action.toUpperCase();
 			char action1 = action.charAt(0);
@@ -39,17 +40,12 @@ public class UserPlayer extends Player{
 				action = JOptionPane.showInputDialog("Stand(S) -- Draw a card(D)");
 			}
 			if(action1== 'D')drawCard();
-			else if(action1== 'S'){
+			else if(action1== 'S'){//Si se queda se pasa acaba su turno 
 				System.out.printf("%s Stand",this.name);
 				passTurn();
 				break;
 			} 
-
-			
 		}
-	
 		return true;
 	}
-
-	
 }
