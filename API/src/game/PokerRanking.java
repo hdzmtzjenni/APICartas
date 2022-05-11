5f7dbdda28eba7de0210bf86cc95c1c178df6883
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-05-11 11:50:08 -0500

    Create PokerRanking.java

---
+++ API/src/game/PokerRanking.java
@@ -0,0 +1,24 @@
+package game;
+
+public enum PokerRanking {
+    RoyalFlush(10),
+    StraightFlush(9),
+    FourOfAKind(8),
+    FullHouse(7),
+    Flush(6),
+    Straight(5),
+    ThreeOfAKind(4),
+    TwoPair(3),
+    OnePair(2),
+    HighCard(1);
+    
+    private int ranking;
+    
+    private PokerRanking(int ranking) {
+        this.ranking=ranking;
+    }
+    public int getRanking() {
+        return this.ranking;
+    }    
+    
+}
