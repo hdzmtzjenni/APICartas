be41d37405e47d893bf35ca969ec3451cec5f917
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-05-11 13:10:45 -0500

    Update PokerPlayer.java

--- API/src/game/PokerPlayer.java
+++ API/src/game/PokerPlayer.java
@@ -7,7 +7,7 @@ public class PokerPlayer extends Player {
 	@Override
 	public int sumOfHand() {
 		// TODO Auto-generated method stub
-		PokerRanking PR;
+		PokerRanking PR =PokerRanking.HighCard;
 		Card c1; 
 		Card c2;
 		Card c3;
@@ -29,7 +29,6 @@ public class PokerPlayer extends Player {
 		// Royal Flush 
 		if (this.sameSuit()) {
 			if(c1.getValue() == 1 && c2.getValue() == 10  && c3.getValue() == 11  && c4.getValue() == 12  && c5.getValue() == 13 ) PR = PokerRanking.RoyalFlush;
-	
 		}
 		
 		// Straight Flush 
@@ -38,27 +37,36 @@ public class PokerPlayer extends Player {
 		}
 		
 		// Four Of A Kind
-			if(c1.getValue() != c2.getValue()) {
-				if(c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue() && c4.getValue() == c5.getValue()) PR = PokerRanking.FourOfAKind;
-			} else if (c1.getValue() == c2.getValue() && c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue()) PR = PokerRanking.FourOfAKind;
+		if(c1.getValue() != c2.getValue()) {
+			if(c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue() && c4.getValue() == c5.getValue()) PR = PokerRanking.FourOfAKind;
+		} else if (c1.getValue() == c2.getValue() && c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue()) PR = PokerRanking.FourOfAKind;
 			
 		// Full House 
-	
-				
-		
+		if(c1.getValue() == c2.getValue() && c3.getValue() ==c4.getValue() && c4.getValue()==c5.getValue()){
+			PR = PokerRanking.FullHouse;
+		} else if(c1.getValue() == c2.getValue() && c2.getValue() ==c3.getValue() && c4.getValue()==c5.getValue()) PR = PokerRanking.FullHouse;
+
+		//Flush
+		if(this.sameSuit()){
+			PR = PokerRanking.Flush;
+		}
+
+		//Straight
+		if(c2.getValue()== c1.getValue()+1 && c3.getValue()==c2.getValue()+1 && c4.getValue()==c3.getValue()+1 && c5.getValue() ==c4.getValue())
+			PR = PokerRanking.Straight;
 		
 		// 
 		
-		return 0;
+		return PR.getRanking();
 		
 		
 	}
 	
 
 	@Override
-	public Boolean play() {
+	public void play() {
 		// TODO Auto-generated method stub
-		return null;
+
 	}
 	
 	
