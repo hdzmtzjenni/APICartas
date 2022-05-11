0d6838771e5fcbaffc1332161efb45cefbf47251
Gabriel Olvera <gavoxolvera@gmail.com>
2022-05-11 11:37:21 -0500

    elimination of override statments

--- API/src/game/PokerPlayer.java
+++ API/src/game/PokerPlayer.java
@@ -4,28 +4,39 @@ package game;
 public class PokerPlayer extends Player {
 
 	@Override
+	public int sumOfHand() {
+		// TODO Auto-generated method stub
+
+		Card c1; 
+		Card c2;
+		Card c3;
+		Card c4;
+		Card c5;
+	
+		//p_hand sort 
+		
+		
+		// card division
+		
+		c1 = p_hand.get(0);
+		c2 = p_hand.get(1);
+		c3 = p_hand.get(2);
+		c4 = p_hand.get(3);
+		c5 = p_hand.get(4);
+		
+		// Royal Flush 
+		
+		return 0;
+	}
+	
+
+	@Override
 	public Boolean play() {
 		// TODO Auto-generated method stub
-		return false;
-		
+		return null;
 	}
+	
+	
 
-	@Override
-	public void drawCard() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void putCardInTrashPile() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void passTurn() {
-		// TODO Auto-generated method stub
-		
-	}
-    
+	
 }
