1edef23c1bf3dac08210fcc0a693797ceb6d4596
Gabriel Olvera <gavoxolvera@gmail.com>
2022-05-14 08:32:43 -0500

    Update PokerPlayer.java
    Added MaxPlayable Value per hamd

--- API/src/game/PokerPlayer.java
+++ API/src/game/PokerPlayer.java
@@ -24,64 +24,126 @@ public class PokerPlayer extends Player {
 		c4 = p_hand.get(3);
 		c5 = p_hand.get(4);
 		
+		// default value of MaxPlayableValue
+		this.maxPlayableValue = c5.getValue();
+		
 		// Royal Flush 
 		if (this.sameSuit()) {
 			if(c1.getValue() == 1 && c2.getValue() == 10  && c3.getValue() == 11  && c4.getValue() == 12  && c5.getValue() == 13 ) PR = PokerRanking.RoyalFlush;
+			//MaxPlayableValue
+			this.maxPlayableValue = 13;
 		}
 		
 		// Straight Flush 
 		if (this.sameSuit()) {
 			if(c2.getValue() == (c1.getValue()+1)  && c3.getValue() == (c1.getValue()+2)  && c4.getValue() == (c1.getValue()+3)  && c5.getValue() == (c1.getValue()+4)) PR = PokerRanking.StraightFlush;
+			//MaxPlayableValue
+			this.maxPlayableValue = c5.getValue();
 		}
 		
 		// Four Of A Kind
 		if(c1.getValue() != c2.getValue()) {
 			if(c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue() && c4.getValue() == c5.getValue()) PR = PokerRanking.FourOfAKind;
+			//MaxPlayableValue
+			this.maxPlayableValue = c2.getValue();
 		} else if (c1.getValue() == c2.getValue() && c2.getValue() == c3.getValue() && c3.getValue() == c4.getValue()) PR = PokerRanking.FourOfAKind;
+			//MaxPlayableValue
+			this.maxPlayableValue = c1.getValue();
 			
 		// Full House 
 		if(c1.getValue() == c2.getValue() && c3.getValue() ==c4.getValue() && c4.getValue()==c5.getValue()){
+			//MaxPlayableValue
+			this.maxPlayableValue = c3.getValue(); // where the three of a kind is 
+			
 			PR = PokerRanking.FullHouse;
-		} else if(c1.getValue() == c2.getValue() && c2.getValue() ==c3.getValue() && c4.getValue()==c5.getValue()) PR = PokerRanking.FullHouse;
-
+		} else if(c1.getValue() == c2.getValue() && c2.getValue() ==c3.getValue() && c4.getValue()==c5.getValue()) {
+			PR = PokerRanking.FullHouse;
+			//MaxPlayableValue
+			this.maxPlayableValue = c1.getValue(); // where the three of a kind is
+		}
 		//Flush
 		if(this.sameSuit()){
 			PR = PokerRanking.Flush;
+			//MaxPlayableValue
+			this.maxPlayableValue = c5.getValue();
 		}
 
 		//Straight
-		if(c2.getValue()== c1.getValue()+1 && c3.getValue()==c2.getValue()+1 && c4.getValue()==c3.getValue()+1 && c5.getValue() ==c4.getValue())
+		if(c2.getValue()== c1.getValue()+1 && c3.getValue()==c2.getValue()+1 && c4.getValue()==c3.getValue()+1 && c5.getValue() ==c4.getValue()+1) {
 			PR = PokerRanking.Straight;
-		
+			//MaxPlayableValue
+			this.maxPlayableValue = c5.getValue();
+			
+		}
 		// Three of a Kind
 		if(c1.getValue()==c2.getValue() && c2.getValue()==c3.getValue()){
 			PR = PokerRanking.ThreeOfAKind;
+			PR = PokerRanking.Straight;
+			//MaxPlayableValue
+			this.maxPlayableValue = c1.getValue();
 		} else if(c2.getValue()==c3.getValue() && c3.getValue()==c4.getValue()){
 			PR = PokerRanking.ThreeOfAKind;
-		} else if(c3.getValue()==c4.getValue() && c4.getValue()==c5.getValue())
+			//MaxPlayableValue
+			this.maxPlayableValue = c2.getValue();
+		} else if(c3.getValue()==c4.getValue() && c4.getValue()==c5.getValue()) {
 			PR = PokerRanking.ThreeOfAKind;
+			//MaxPlayableValue
+			this.maxPlayableValue = c3.getValue();
+		}
 
 		//Two Pair
 		if(c1.getValue()!= c2.getValue()){
 			if(c2.getValue()==c3.getValue()){
-				if(c4.getValue()==c5.getValue()) PR = PokerRanking.TwoPair;
+				if(c4.getValue()==c5.getValue() && c3.getValue() != c4.getValue()) {
+					PR = PokerRanking.TwoPair;
+					//MaxPlayableValue
+					this.maxPlayableValue = c4.getValue();					
+				}
 			}	
 		}else if(c1.getValue()== c2.getValue()) {
 			if (c3.getValue()!=c4.getValue()) {
-				if(c4.getValue() ==c5.getValue())  PR = PokerRanking.TwoPair;
-			}else if(c3.getValue() == c4.getValue())PR = PokerRanking.TwoPair;
+				if(c4.getValue() ==c5.getValue()) {
+					PR = PokerRanking.TwoPair;
+					//MaxPlayableValue
+					this.maxPlayableValue = c4.getValue();
+				}
+			}else if(c3.getValue() == c4.getValue()) {
+				PR = PokerRanking.TwoPair;
+				//MaxPlayableValue
+				this.maxPlayableValue = c3.getValue();
+			}
 		}
 		
 		//Pair
 		if(c1.getValue()!= c2.getValue()){
 			if(c2.getValue()!=c3.getValue()){
 				if(c3.getValue()!=c4.getValue()){
-					if(c4.getValue()== c5.getValue()) PR = PokerRanking.OnePair;
-				}else if(c3.getValue()==c4.getValue()) PR = PokerRanking.OnePair;
-			}else if(c2.getValue()== c3.getValue()) PR = PokerRanking.OnePair;
-		}else if(c1.getValue()== c2.getValue())PR = PokerRanking.OnePair;
+					if(c4.getValue()== c5.getValue()) {
+						PR = PokerRanking.OnePair;
+						//MaxPlayableValue
+						this.maxPlayableValue = c5.getValue();
+						
+					}
+				}else if(c3.getValue()==c4.getValue()) {
+					PR = PokerRanking.OnePair;
+					//MaxPlayableValue
+					this.maxPlayableValue = c4.getValue();
+				}
+			}else if(c2.getValue()== c3.getValue()) {
+				PR = PokerRanking.OnePair;
+				//MaxPlayableValue
+				this.maxPlayableValue = c3.getValue();
+			}
+		}else if(c1.getValue()== c2.getValue()) {
+			PR = PokerRanking.OnePair;
+			//MaxPlayableValue
+			this.maxPlayableValue = c2.getValue();
+		}
+		
+		
 
 		return PR.getRanking();
+		
 	}
 	
 
