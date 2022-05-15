13a1af8ce1b25c5975f720058c83e6e95769919a
Gabriel Olvera <gavoxolvera@gmail.com>
2022-05-15 18:43:09 -0500

    Comms Inteligent

--- API/src/game/IntelligentPlayer.java
+++ API/src/game/IntelligentPlayer.java
@@ -1,29 +1,29 @@
 package game;
 
-
+// Subclass of BlackPlayer that emulates the playstile of an Intelligent Player of Type BlackJackPlayer 
 public class IntelligentPlayer extends BlackJackPlayer {
 	
-	public IntelligentPlayer(){
+	public IntelligentPlayer(){ //Public Constructor 
 
 	}
 	
-	// Funcion que juega a Black Jack y determina si vale la pena seguir jugando o no dependiendo de sus cartas actuales 
+	// Function of Play that also evaluates probabilities depending on Sum of hands if it's convinient to keep grabbing 
 	public void play() {
 		System.out.printf("\n\n----------------- Player %s -----------------",this.name);
 		
-		while (!handReady) {//Mientras el jugador no termine su turno
+		while (!handReady) {//While player is not finished with it's turn 
 			System.out.printf("\nHand: %d\n",this.sumOfHand());
 			System.out.println(this.getP_hand());
 			int getRandomValue = (int) (Math.random()*(100-1)) + 1;
 		
-			if (this.sumOfHand()==21) {//Si la suma de sus cartas es 21 el jugador gana y se acaba su turno
+			if (this.sumOfHand()==21) {//If the sum of Player is 21 he wins
 				System.out.printf("%s tiene BlackJack",this.name);
 				this.handReady = true;
 				this.turn++;
 				break;
 			}
 
-			if (this.sumOfHand()>21) {//Si la suma es más de 21 el jugador pierde y se acaba su turno
+			if (this.sumOfHand()>21) {//If the sum is more than 21 he losses 
 				System.out.printf("Tiene más de 21, %s pierde ",this.name);
 				this.handReady=true;
 				this.turn++;
@@ -32,7 +32,7 @@ public class IntelligentPlayer extends BlackJackPlayer {
 
 			//
 			
-			//Hay un 80% de probabilidad de que el jugador agarre una carta
+			//Probabilities of grabbing card depending in sum of hand 
 			if(this.sumOfHand()<=11) drawCard();
 			if(this.sumOfHand()==12){
 				if (getRandomValue<=90) {
@@ -42,7 +42,7 @@ public class IntelligentPlayer extends BlackJackPlayer {
 					break;
 				}
 			}
-			if(this.sumOfHand()==13 || this.sumOfHand()==14){
+			if(this.sumOfHand()==13 || this.sumOfHand()==14){ // sum of hand between 13 and 14 
 				if (getRandomValue<=70) {
 					drawCard();
 				}else{
@@ -50,7 +50,7 @@ public class IntelligentPlayer extends BlackJackPlayer {
 					break;
 				}
 			}
-			if(this.sumOfHand()==15 || this.sumOfHand()==16){
+			if(this.sumOfHand()==15 || this.sumOfHand()==16){ // sum of hand betwwen 15 and 16
 				if (getRandomValue<=60) {
 					drawCard();
 				}else{
@@ -58,7 +58,7 @@ public class IntelligentPlayer extends BlackJackPlayer {
 					break;
 				}
 			}
-			if(this.sumOfHand()>=17 && this.sumOfHand()<=18){
+			if(this.sumOfHand()>=17 && this.sumOfHand()<=18){ // sum of hand between 17 and 18 
 				if (getRandomValue<=20) {
 					drawCard();
 				}else{
@@ -66,11 +66,11 @@ public class IntelligentPlayer extends BlackJackPlayer {
 					break;
 				}
 			}
-			if(this.sumOfHand()>=19 && this.sumOfHand()<=20){
+			if(this.sumOfHand()>=19 && this.sumOfHand()<=20){ // sum of hand between 19 and 20 
 				if (getRandomValue<=5) {
 					drawCard();
 				}else{
-					passTurn();
+					passTurn(); // else passes turn 
 					break;
 				}
 			}
