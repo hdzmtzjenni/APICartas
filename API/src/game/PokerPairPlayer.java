37adfe9a321fab1d9fca4e040d707e2d13376c27
Gabriel Olvera <gavoxolvera@gmail.com>
2022-05-15 19:31:02 -0500

    Comms poker pair Player

--- API/src/game/PokerPairPlayer.java
+++ API/src/game/PokerPairPlayer.java
@@ -2,15 +2,17 @@ package game;
 
 import java.util.Collections;
 
+// SubClass of Poker Player that always is looking to generate the next pair , if not it always passes 
+
 public class PokerPairPlayer extends PokerPlayer{
     
-    public PokerPairPlayer() {
+    public PokerPairPlayer() { // public Constructor 
     }
 
     @Override
 	public void play() {
     	System.out.printf("\n\n----------------- Player %s -----------------",this.name);
-    	while (!handReady) {//Mientras el jugador no termine su turno
+    	while (!handReady) {// until Player not finish turn 
 			//The cards inTable are shown in console 
             System.out.println("\nIn Table");
             System.out.println(PokerGame.getInTable());
@@ -20,12 +22,12 @@ public class PokerPairPlayer extends PokerPlayer{
 			Collections.sort(this.getP_hand());
 			System.out.println(this.getP_hand());
 
-			int lastcard = 5;
-			int value =6;
-			int indexInTable = 1;
-			int indexInHand =1;
+			int lastcard = 5; // Last card to grab
+			int value =6; // Value 
+			int indexInTable = 1; // Index of table to swap
+			int indexInHand =1; // index in hand 
 
-			if (this.sumOfHand()== 2 || this.sumOfHand()==3 || this.sumOfHand()==4 || this.sumOfHand()==8 || this.sumOfHand()==7) {
+			if (this.sumOfHand()== 2 || this.sumOfHand()==3 || this.sumOfHand()==4 || this.sumOfHand()==8 || this.sumOfHand()==7) { // if you already have a nice hand, Hand ready and pass turn 
 				if (this.getMaxPlayableCard().getValue() >9) {
 					System.out.printf("%s Hand Ready ",this.name);
 					passTurn();
@@ -34,31 +36,31 @@ public class PokerPairPlayer extends PokerPlayer{
 				
 			}
 			
-			for (Card c : PokerGame.getInTable()) {
+			for (Card c : PokerGame.getInTable()) { // get in table and check for pairs 
 				indexInHand=1;
 				for (Card ca : p_hand) {
-					if( ca.getValue() == c.getValue()) {
-						if (ca.getValue()>=value) {
+					if( ca.getValue() == c.getValue()) { // Compares values 
+						if (ca.getValue()>=value) { // Saves Value 
 							if (lastcard == indexInHand) {
-								PokerGame.swapCards(indexInTable, (lastcard-1), getP_hand());
+								PokerGame.swapCards(indexInTable, (lastcard-1), getP_hand()); // swaps from last card minus if it is lastcard index 
 								this.turn++;
 								return;
 							}
 							else{
-								PokerGame.swapCards(indexInTable, lastcard, getP_hand());
+								PokerGame.swapCards(indexInTable, lastcard, getP_hand()); // swaps from last card 
 								this.turn++;
 								return;
 							}
 						}
 					
 					}
-					indexInHand++;
+					indexInHand++; // adds to index of hand
 				}
-				indexInTable ++;
+				indexInTable ++; // Adds to index of table 
 			}
 			
-			System.out.printf("%s Hand Ready ",this.name);
-			passTurn();
+			System.out.printf("%s Hand Ready ",this.name); // hand ready 
+			passTurn(); // Pass turn 
 			break;
 			
     	}
